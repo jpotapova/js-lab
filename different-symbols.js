9980d9aff9f2cd9205f0379e7b1e3a7b0a93a08e
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-03-03 23:11:55 +0200

    list all different symbols found in a string

--- different-symbols.js
+++ different-symbols.js
@@ -1,5 +1,14 @@
 function differentSymbols(s) {
-  return '';
+  var result = '';
+  var l = s.length;
+  var letter;
+  for (var i = 0; i < l; i++) {
+    letter = s[i];
+    if (!result.includes(letter)) {
+      result = result + letter;
+    }
+  }
+  return result;
 }
 
 module.exports = differentSymbols;
