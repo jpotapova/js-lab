72bf8400382162e580afcebd500fb93ac79b1a86
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-03-04 09:44:12 +0200

    array sum trivial case

---
+++ __tests__/array-sum.test.js
@@ -0,0 +1,18 @@
+const arraySum = require('../array-sum');
+describe('Array sum', () => {
+  it('returns 0 for an empty array', () => {
+    expect(arraySum([])).toBe(0);
+  });
+  // it('generates "a" for a string "a"', () => {
+  //   expect(differentSymbols('a')).toEqual('a');
+  // });
+  // it('generates "ab" for a string "ab"', () => {
+  //   expect(differentSymbols('ab')).toEqual('ab');
+  // });
+  // it('generates "abc" for a string "abc"', () => {
+  //   expect(differentSymbols('abc')).toEqual('abc');
+  // });
+  // it('generates "abc" for a string "abaca"', () => {
+  //   expect(differentSymbols('abaca')).toEqual('abc');
+  // });
+});
---
+++ array-sum.js
@@ -0,0 +1,5 @@
+function arraySum(n, arr) {
+  return 0;
+}
+
+module.exports = arraySum;
