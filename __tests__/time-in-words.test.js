f4710dec5e0ee19f78db51ea0117a1be169c46ac
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-03-07 22:59:44 +0200

    0 and 15 mins

--- __tests__/time-in-words.test.js
+++ __tests__/time-in-words.test.js
@@ -1,6 +1,45 @@
 const timeInWords = require('../time-in-words');
-describe('Time in words', () => {
-  it('returns 0', () => {
-    expect(timeInWords(0, 0)).toBe(0);
+describe('Time in words returns', () => {
+  it("five o' clock for 5:00", () => {
+    expect(timeInWords(5, 0)).toBe("five o' clock");
+  });
+  it("one o' clock for 1:00", () => {
+    expect(timeInWords(1, 0)).toBe("one o' clock");
+  });
+  it("two o' clock for 2:00", () => {
+    expect(timeInWords(2, 0)).toBe("two o' clock");
+  });
+  it("three o' clock for 3:00", () => {
+    expect(timeInWords(3, 0)).toBe("three o' clock");
+  });
+  it("four o' clock for 4:00", () => {
+    expect(timeInWords(4, 0)).toBe("four o' clock");
+  });
+  it("six o' clock for 6:00", () => {
+    expect(timeInWords(6, 0)).toBe("six o' clock");
+  });
+  it("seven o' clock for 7:00", () => {
+    expect(timeInWords(7, 0)).toBe("seven o' clock");
+  });
+  it("eight o' clock for 8:00", () => {
+    expect(timeInWords(8, 0)).toBe("eight o' clock");
+  });
+  it("nine o' clock for 9:00", () => {
+    expect(timeInWords(9, 0)).toBe("nine o' clock");
+  });
+  it("ten o' clock for 10:00", () => {
+    expect(timeInWords(10, 0)).toBe("ten o' clock");
+  });
+  it("eleven o' clock for 11:00", () => {
+    expect(timeInWords(11, 0)).toBe("eleven o' clock");
+  });
+  it("twelve o' clock for 12:00", () => {
+    expect(timeInWords(12, 0)).toBe("twelve o' clock");
+  });
+  it('quarter past five for 5:15', () => {
+    expect(timeInWords(5, 15)).toBe('quarter past five');
+  });
+  it('quarter past six for 6:15', () => {
+    expect(timeInWords(6, 15)).toBe('quarter past six');
   });
 });
--- time-in-words.js
+++ time-in-words.js
@@ -1,6 +1,33 @@
 function timeInWords(h, m) {
-  // Complete this function
-  return 0;
+  var result = '';
+  var nrToWord = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'half'
+  };
+  if (m === 0) {
+    result = nrToWord[h] + " o' clock";
+  } else if (m === 15) {
+    result = 'quarter past ' + nrToWord[h];
+  }
+  return result;
 }
 
 module.exports = timeInWords;
