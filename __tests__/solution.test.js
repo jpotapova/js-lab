db510b47b6ceff09f4b267f0544fbf4791207fec
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-03-09 23:19:23 +0200

    initial setup

--- __tests__/solution.test.js
+++ __tests__/solution.test.js
@@ -1,42 +1,8 @@
 const solution = require('../solution');
 var s = new solution();
 
-describe('same length', () => {
-  it('10 11', () => {
-    expect(s.sameLength(10, 11)).toBe(true);
-  });
-  it('10 1', () => {
-    expect(s.sameLength(10, 1)).toBe(false);
-  });
-});
-
-describe('split by length', () => {
+describe('Gift boxes', () => {
   it('case 1', () => {
-    expect(s.splitByLength([1, 2, 3, 10, 13, 23, 31, 32])).toEqual([0, 3]);
-  });
-});
-
-describe('is recycled', () => {
-  xit('case 1', () => {
-    expect(s.isRecycledInt(1, 1)).toBe(false);
-    expect(s.isRecycledInt(11, 11)).toBe(false);
-    expect(s.isRecycledInt(111, 111)).toBe(false);
-  });
-  it('case 2', () => {
-    expect(s.isRecycledInt(12, 21)).toBe(true);
-    expect(s.isRecycledInt(113, 131)).toBe(true);
-    expect(s.isRecycledInt(1234, 3412)).toBe(true);
-  });
-});
-
-describe('solution', () => {
-  it('case1', () => {
-    expect(s.uniqueRecycledPairs([2, 3, 10, 1, 13, 31, 23, 32])).toBe(2);
-  });
-  it('case2', () => {
-    expect(s.uniqueRecycledPairs([2345, 3452, 10, 1, 13, 31, 23, 32])).toBe(3);
-  });
-  it('case3', () => {
-    expect(s.uniqueRecycledPairs([23, 32, 23, 32])).toBe(1);
+    expect(s.giftBoxes('ab', 'abab')).toBe(2);
   });
 });
--- solution.js
+++ solution.js
@@ -1,84 +1,12 @@
 function solution() {
-  function uniqueRecycledPairs(A) {
+  function giftBoxes(g, c) {
     /*
-	     * Return the number of unique recycled pairs present in the array.
-	     */
-    var count = 0;
-    var arr = A.sort(function compareNumbers(a, b) {
-      return a - b;
-    }).filter(function(elem, pos, arr) {
-      return arr.indexOf(elem) == pos;
-    });
-    var n = arr.length;
-    var lengthChanges = splitByLength(arr);
-    var lengthChangesCount = lengthChanges.length - 1;
-    for (var l = 0; l < lengthChangesCount; l++) {
-      let iFrom = lengthChanges[l]; // first index to include
-      let iTo = lengthChanges[l + 1] - 1; // last index to include
-      // compare all the pairs within this part of array
-      count = count + compareInts(iFrom, iTo, arr);
-    }
-    {
-      let iFrom = lengthChanges[lengthChangesCount]; // first index to include
-      let iTo = n - 1; // last index to include
-      count = count + compareInts(iFrom, iTo, arr);
-      // compare all the pairs within this part of array
-    }
-    return count;
-  }
-
-  function compareInts(iFrom, iTo, arr) {
-    var count = 0;
-    var max = arr[iFrom].toString().length - 1;
-    for (var i = iFrom; i < iTo; i++) {
-      var c = 0;
-      for (var j = i + 1; j <= iTo; j++) {
-        if (isRecycledInt(arr[i], arr[j])) {
-          c++;
-          count++;
-          if (c === max) j = iTo + 1;
-        }
-      }
-    }
-    return count;
-  }
-
-  function sameLength(a, b) {
-    return a.toString().length === b.toString().length;
-  }
-
-  function splitByLength(arr) {
-    var l = arr.length;
-    var lengthChanges = [0];
-    for (var i = 0; i < l - 1; i++) {
-      if (!sameLength(arr[i], arr[i + 1])) {
-        lengthChanges.push(i + 1);
-      }
-    }
-    return lengthChanges;
-  }
-
-  function isRecycledInt(int1, int2) {
-    if (int1 === int2) return false;
-    var int1s = int1.toString();
-    var int2s = int2.toString();
-    var l = int1s.length;
-    var isPair = false;
-    for (var i = 1; i < l; i++) {
-      let s = int1s.substr(i) + int1s.substr(0, i);
-      if (s === int2s) {
-        isPair = true;
-        i = l;
-      }
-    }
-    return isPair;
+     * Return the number of gift boxes that can be packaged.
+     */
   }
 
   return {
-    uniqueRecycledPairs: uniqueRecycledPairs,
-    sameLength: sameLength,
-    splitByLength: splitByLength,
-    isRecycledInt: isRecycledInt
+    giftBoxes: giftBoxes
   };
 }
 
